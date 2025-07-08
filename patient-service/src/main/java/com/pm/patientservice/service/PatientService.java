93e4aaba181bbc0f96b9bbfae630b3e0bc158df1
ZayedShahcode <reachzayed@proton.me>
2025-07-08 18:52:39 +0530

    grpc patient-service

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -16,11 +17,14 @@ import java.util.UUID;
 
 @Service
 public class PatientService {
-    private final PatientRepository patientRepository;
 
-    public PatientService(PatientRepository patientRepository)
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient)
     {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient =  billingServiceGrpcClient;
 
     }
 
@@ -42,6 +46,9 @@ public class PatientService {
         Patient newPatient = patientRepository.save(
                 PatientMapper.toModel(patientRequestDTO)
         );
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(),newPatient.getEmail());
+
         return PatientMapper.toDTO(newPatient);
     }
 
@@ -63,4 +70,6 @@ public class PatientService {
     public void deletePatient(UUID id){
         patientRepository.deleteById(id);
     }
+
+
 }
